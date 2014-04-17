57caa9094077350181127d79984f0f8d181f97e9
Ronald Victorino <ron@nthgensoftware.com>
2014-04-17 17:25:29 -0400

    Added navbar with some js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+  // Check the initial Poistion of  Sticky Header
+  var landingPageMiddle = $('#aboutMe').offset().top * 0.50;
+  var landingPageBottom = $('#aboutMe').offset().top;
+
+  console.log(landingPageMiddle);
+  console.log(landingPageBottom);
+
+  $(window).scroll(function(){
+    if($(window).scrollTop() < landingPageBottom) {
+      $('#navbar').css('position', 'static');
+      $('#navbar').css('margin-left', '0px');
+      var opacity = $(window).scrollTop() / landingPageBottom;
+      $('#navbar').css('opacity', opacity);
+    } else {
+      $('#navbar').css('position', 'fixed');
+      $('#navbar').css('top', '5px');
+      $('#navbar').css('margin-left', '-5px');
+      $('#navbar').css('opacity', '1');
+    }
+  }); 
+
+ });
