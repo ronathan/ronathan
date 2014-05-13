64335c4ec6e75478f1b125559fff0db85b16bc2a
ronathan <ronaldvictorino@gmail.com>
2014-05-13 04:40:49 +0000

    Messed up everything. Not sure if I should even be committing this.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,12 @@
 $(function(){
   // Check the initial Poistion of  Sticky Header
   var landingPageMiddle = $('#aboutMe').offset().top * 0.50;
-  var landingPageBottom = $('#aboutMe').offset().top;
+  var projectsPageOffset = $('#projects-display').offset().top
 
+  // Effects for different sections on scroll
   $(window).scroll(function() {
-    if($(window).scrollTop() < landingPageBottom) {
-      $('#navbar').css('position', 'static');
-      $('#navbar').css('margin-left', '0px');
-      var opacity = $(window).scrollTop() / landingPageBottom;
-      $('#navbar').css('opacity', opacity);
-    } else {
-      $('#navbar').css('position', 'fixed');
-      $('#navbar').css('top', '5px');
-      $('#navbar').css('opacity', '0.9');
-    }
 
+    // Animate at middle of about me section
     if($(window).scrollTop() >= landingPageMiddle) {
       $('.aboutme-item').each(function() {
 
@@ -27,11 +19,13 @@ $(function(){
 
       }).parent().promise().done();
     } 
-    // else if($(window).scrollTop() < landingPageMiddle ) {
-    //   $('.hobby-item').css('opacity', '0');
-    //   $('.hobby-item').removeClass('translateLeft');
-    // }
 
+    // Animate arrows in when you reach projects page
+    if( $(window).scrollTop() >= projectsPageOffset) {
+      $('.project-arrows').fadeIn();
+    } else {
+      $('.project-arrows').fadeOut();
+    }
   }); 
 
 });
