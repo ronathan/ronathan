0de77298c0a2faca9eadef0bc4ae008a1ab52f3c
Ronald Victorino <ron@nthgensoftware.com>
2014-05-23 11:18:01 -0400

    Added functionality to change between projects.

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $(function(){
 
         $this.parent().queue(function(n) {
           $this.addClass('translateLeft');
-          $this.animate({opacity:1}, 300, n);
+          $this.animate({opacity:1}, 100, n);
         });
 
       }).parent().promise().done();
@@ -28,5 +28,25 @@ $(function(){
     //   $('.project-arrows').stop().animate({opacity:0}, 400);
     // }
   }); 
+
+  var projectCounter = 1;
+
+  $('#project-right-arrow').click(function() {
+    projectCounter++;
+    if(projectCounter == 5) {
+      projectCounter = 1;
+    }
+    $('#inner-display').removeClass();
+    $('#inner-display').addClass('project' + projectCounter.toString());
+  });
+
+  $('#project-left-arrow').click(function() {
+    projectCounter--;
+    if(projectCounter == 0) {
+      projectCounter = 4;
+    }
+    $('#inner-display').removeClass();
+    $('#inner-display').addClass('project' + projectCounter.toString());
+  });
 
 });
