729f6f9b18ea663ded45e06b188d4700fa36aa39
ronathan <ronaldvictorino@gmail.com>
2014-06-04 04:13:09 +0000

    Added waypoints.

--- js/main.js
+++ js/main.js
@@ -3,11 +3,9 @@ $(function(){
   var landingPageMiddle = $('#aboutMe').offset().top * 0.50;
   var projectsPageOffset = $('#projects').offset().top
 
-  // Effects for different sections on scroll
-  $(window).scroll(function() {
-
-    // Animate at middle of about me section
-    if($(window).scrollTop() >= landingPageMiddle) {
+  $('#aboutMe').waypoint(function(direction) {
+    console.log('this was hit');
+    if(direction === "down") {
       $('.aboutme-item').each(function() {
 
         var $this = $(this);
@@ -18,16 +16,8 @@ $(function(){
         });
 
       }).parent().promise().done();
-    } 
-
-    // Animate arrows in when you reach projects page
-    // if( $(window).scrollTop() >= projectsPageOffset) {
-    //   $('.project-arrows').stop().animate({opacity:1}, 400);
-    //   console.log("THIS WAS HIT");
-    // } else if($(window).scrollTop() < projectsPageOffset) {
-    //   $('.project-arrows').stop().animate({opacity:0}, 400);
-    // }
-  }); 
+    }
+  }, {offset: '50%'});
 
   var projectCounter = 1;
 
